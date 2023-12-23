55d2427fdf992cc5fb5439bba33d90d0d9fc4302
samahmud49@gmail.com <samahmud49@gmail.com>
2023-12-23 11:16:28 +0600

    centi Second update

--- main.js
+++ main.js
@@ -44,10 +44,12 @@ setInterval(timeZone,1000);
 let myhour = document.getElementById('myhour');
 let myminute = document.getElementById('myminute');
 let mysec = document.getElementById('mysec');
+let mycenti = document.getElementById('mycenti');
 
 let hr5 = 0;
 let min5 = 0;
 let sec5 = 0;
+let centi5 = 0;
 
 let stopTime = true;
 
@@ -56,6 +58,7 @@ function startWatch() {
 	if(stopTime == true){
 		stopTime = false;
 		timer() ;
+		autocall();
 	}
 }
 
@@ -72,12 +75,13 @@ function restWatch() {
 	myhour.innerText = '00';
 	myminute.innerText = '00';
 	mysec.innerText = '00';
-	
+	mycenti.innerText = '00'
 	stopTime = true;
 	
 	hr5 = 0;
 	min5 = 0;
 	sec5 = 0;
+	centi5 = 0;
 }
 
 
@@ -108,4 +112,23 @@ function timer() {
 		setTimeout(timer,1000);
 	}
 	
+}
+
+
+function autocall() {
+	
+	if(stopTime == false){
+		let d = new Date();
+	let d1 = d.getMilliseconds();
+d2 = d1 / 10;
+	d3 = Math.floor(d2);
+	
+	
+	
+	var formattedNumber = ("0" + d3).slice(-2);
+	document.getElementById('mycenti').innerText = formattedNumber;
+	
+	}
+	
+	setTimeout(autocall,5);
 }
